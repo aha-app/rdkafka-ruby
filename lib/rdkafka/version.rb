fa810e5c6c9eee23479cf67dd56c8c0214edf66a
Alex Bartlow <bartlowa@gmail.com>
2021-10-13 09:07:07 -0400

    Tweak version

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,5 +1,5 @@
 module Rdkafka
-  VERSION = "1.3.0.aha"
+  VERSION = "0.8.0.beta.2.aha"
   LIBRDKAFKA_VERSION = "0.0.1.aha"
   LIBRDKAFKA_SOURCE_SHA256 = "02a8893c89d77d02aa092ed3427596344bbfdcb2c9a0cfa9f4a13f59d25cacef"
   LIBRDKAFKA_FORK = "aha-app"
